0597ae3246ea0313a37f989f8eaed32dd38d1d50
BUGYEP <blue.tagyut@gmail.com>
2020-11-09 11:15:37 +0900

    やってみてダメだったので戻す, これ認証情報隠すの無理だな

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const COMETCHAT_CONSTANTS = {
-    APP_ID: process.env.NETLIFY_APP_ID,
-    REGION: process.env.NETLIFY_REGION,
-    AUTH_KEY: process.env.NETLIFY_AUTH_KEY
+    APP_ID: '251613b6eb448b2',
+    REGION: 'us',
+    AUTH_KEY: 'a0e567472b09bcedd33ad347a7cec4349ab22b07'
 }
