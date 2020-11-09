ac4c615f8fdddea082128e9a4b6424d32b9f3d9d
BUGYEP <blue.tagyut@gmail.com>
2020-11-09 11:12:09 +0900

    process.env書き忘れたら終わりだよ

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const COMETCHAT_CONSTANTS = {
-    NETLIFY_APP_ID,
-    NETLIFY_REGION,
-    NETLIFY_AUTH_KEY
+    APP_ID: process.env.NETLIFY_APP_ID,
+    REGION: process.env.NETLIFY_REGION,
+    AUTH_KEY: process.env.NETLIFY_AUTH_KEY
 }
